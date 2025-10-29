bdddeb5325c71d849172dc2a55be534677461927
Wwb-Daniel <danielfffreefire0@gmail.com>
2025-10-29 12:02:47 -0400

    fix: Cambiar a adaptador static para evitar problemas de runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server',
-  adapter: vercel({
-    functionPerRoute: false
-  })
+  output: 'static',
+  adapter: vercel()
 });
